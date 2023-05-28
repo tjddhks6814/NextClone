bdebbdcc32e16441d2b6b4b3dc6f54b9197d58aa
tjddhks6814 <tjddhks6814@naver.com>
2023-05-28 17:15:15 +0900

    Styled-component 추가 & updateBtn 추가

--- pages/todolist/list.tsx
+++ pages/todolist/list.tsx
@@ -1,14 +1,10 @@
-import { useState } from "react"
+import React, { useState } from "react"
 
 const List = () => {
-    const [todo, setTodo] = useState<Array<string>>([]);
-    const [list, setList] = useState<string>("");
+    const [todo, setTodo] = useState<string[]>([]);
+    const [list, setList] = useState("");
 
-    interface deleteBtn {
-        (): () => void;
-    }
-
-    const todoInsert = (e:any) => {
+    const todoInsert = (e:React.MouseEvent<HTMLElement>) => {
         e.preventDefault();
         if(list !== ""){
             setTodo([...todo, list]);
@@ -19,27 +15,42 @@ const List = () => {
         }
     }
 
-    const todoChange = (e:any) => {
+    const todoChange = (e:React.ChangeEvent<HTMLInputElement>) => {
         setList(e.target.value)
     }
 
-    const todoDelete = (e:any) => {
-        const newList =[...todo];
-        newList.splice(e.target.value, 1);
+    const todoDelete = (e:React.MouseEvent<HTMLElement>): void => {
+        const newList = [...todo];
+        newList.splice(Number(e.currentTarget.getAttribute('value')), 1);
         setTodo(newList);
+    }
+
+    const todoUpdate = (e:React.MouseEvent<HTMLButtonElement>) => {
+        const updateValue = e.currentTarget.value;
+        const inputString = prompt("변경 할 내용을 입력하세요", updateValue);
+        const eId =  Number(e.currentTarget.name);
+        
+        if(inputString != null){
+            const newList = [...todo];
+            newList[eId] = inputString;
+            setTodo(newList);
+            alert("변경되었습니다.");
+        }
+        
     }
 
     return(
         <div className="container">
             <h1 style={{textAlign:"center"}}>My Todo List</h1>
             <input type="text" name="todoList" value={list} placeholder="Todo" onChange={todoChange} />
-            <button onClick={todoInsert}>입력</button>
+            <button id="inputBtn" onClick={todoInsert}>입력</button>
             <ul>
             {
                 todo.map((ele,i) => (
                     <li key={i}>
                         <span>{ele}</span>
-                        <button value={i} onClick={todoDelete} style={{marginLeft:"5px"}}>삭제</button>
+                        <button id={"updateBtn"} name={i} value={ele} onClick={todoUpdate}>변경</button>
+                        <button id="delBtn" value={i} onClick={todoDelete}>삭제</button>
                     </li>
                 ))
             }
--- styles/global-style.ts
+++ styles/global-style.ts
@@ -2,10 +2,66 @@ import { createGlobalStyle } from "styled-components";
  
 export const GlobalStyle = createGlobalStyle`
   html {
-    box-sizing: border-box;
+    width: 600px;
+    background-color: #f5f5f5;
+    border-radius: 5px;
+    padding: 20px;
+    text-align:center;
+  }
+  ul{
+    list-style-type: none;
+    padding: 0;
+  }
+  li{
+    display: flex;
+    align-items: center;
+    margin-bottom: 10px;
+  }
+  span{
+    flex: 1;
     font-size: 20px;
-    min-width: 320px;
-    text-align: center;
+  }
+  #delBtn{
+    background-color: #ff6b6b;
+    color: #ffffff;
+    border: none;
+    border-radius: 5px;
+    padding: 5px 10px;
+    margin-left: 5px;
+    font-size: 14px;
+    cursor: pointer;
+    height: 40px;
+  }
+  #updateBtn{
+    background-color: #C4F4Fe;
+    color: #000000;
+    border: none;
+    border-radius: 5px;
+    padding: 5px 10px;
+    margin-left: 5px;
+    font-size: 14px;
+    cursor: pointer;
+    height: 40px;
+    border: solid 1px #ccc;
+  }
+  #inputBtn{
+    background-color: #E2FFAF;
+    color: #000000;
+    border: none;
+    border-radius: 5px;
+    padding: 5px 10px;
+    margin-left: 10px;
+    font-size: 14px;
+    cursor: pointer;
+    height: 40px;
+    border: solid 1px #ccc;
+    
+  }
+  input{
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    font-size: 16px;
   }
   a { cursor: pointer; text-decoration: none; }
 `;
